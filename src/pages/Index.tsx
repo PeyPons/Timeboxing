5ac20bd5b73604045aff1467c0979ae39f6d0e6d
PeyPons <alexanderouteiral@gmail.com>
2025-12-14 00:25:18 +0000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ const Index = () => {
   return (
     <>
       <Helmet>
-        <title>Planificador | ResourceFlow</title>
+        <title>Timeboxing</title>
         <meta name="description" content="Gestiona los recursos de tu equipo SEO con el planificador visual de ResourceFlow" />
       </Helmet>
       <div className="h-screen flex flex-col">
