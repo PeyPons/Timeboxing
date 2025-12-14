5dd8a9734f0edab7bef55efcea2a5abbabc02fc2
PeyPons <alexanderouteiral@gmail.com>
2025-12-14 01:26:13 +0000

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Usamos variables de entorno para no subir claves privadas a GitHub
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
 
